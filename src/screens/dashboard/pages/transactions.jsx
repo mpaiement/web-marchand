010b0d6e70963607290377300eaae971480ef491
laetitiazidane <laetitiazidane18@gmail.com>
2024-06-23 16:45:39 +0100

    fix welcome page

--- src/screens/dashboard/pages/transactions.jsx
+++ src/screens/dashboard/pages/transactions.jsx
@@ -7,7 +7,7 @@ const Transactions = () => {
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/transaction/historique/marchand/Jwz6jEbVEbOO6IT2YzXyuLk5iqP2`);
+        const response = await axios.get(`http://localhost:3000/transaction/historique/marchand/TIijZuSg2YfXvx2pwuLTWA1kjeQ2`);
         setTransactions(response.data);
       } catch (error) {
         console.error('Error fetching transactions:', error);
@@ -15,27 +15,26 @@ const Transactions = () => {
     };
 
     fetchTransactions();
-  }, );
+  }, []);
 
   return (
-    <div>
-      <h1>Transactions</h1>
+    <div className="mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6 text-left">Transaction History</h1>
       <div className="overflow-x-auto">
         <table className="min-w-full bg-white">
           <thead>
             <tr>
-            <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm font-bold text-gray-600 uppercase tracking-wider">Merchant</th>
-              <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm font-bold text-gray-600 uppercase tracking-wider">User</th>
-             
-              <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm font-bold text-gray-600 uppercase tracking-wider">Amount</th>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm font-bold text-gray-600 uppercase tracking-wider">Merchant</th>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm font-bold text-gray-600 uppercase tracking-wider">User</th>
+              <th className="px-6 py-3 border-b-2 border-gray-300 text-center text-sm font-bold text-gray-600 uppercase tracking-wider">Amount</th>
             </tr>
           </thead>
           <tbody>
             {transactions.map((transaction, index) => (
               <tr key={index} className="hover:bg-gray-100">
-                 <td className="px-6 py-4 whitespace-nowrap">{transaction.marchandname}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{transaction.username}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{transaction.amount1}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-center">{transaction.marchandname}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-center">{transaction.username}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-semibold text-[#0D775E] font-bold text-center">{transaction.amount1} DA</td>
               </tr>
             ))}
           </tbody>
--- src/screens/typeAuth/index.jsx
+++ src/screens/typeAuth/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import '../../App.css';
 import Login from '../authentification/Login';
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import Signup from '../inscription/Signup';
-import {Footer} from '../../layout/Footer';
-const index = () => {
+import { Footer } from '../../layout/Footer';
+import { useNavigate } from 'react-router-dom';
+
+const Index = () => {
   const [content, setContent] = useState(null);
   const [isContentVisible, setIsContentVisible] = useState(false);
+  const navigate = useNavigate();
 
   const handleLoginClick = () => {
     setContent(<Login />);
@@ -18,47 +21,62 @@ const index = () => {
     setIsContentVisible(true);
   };
 
-  return (
-    <div>
-    <div className="container">
-    {/* Div pour l'image d'arrière-plan floutée */}
-    <div className="background-image  blur "></div>
+  const handleBackClick = () => {
+    setContent(null);
+    setIsContentVisible(false);
+  };
 
-    {/* Contenu net */}
-    <div className={`content ${isContentVisible ? "push-left" : ""}`}>
-      <div className="content-inner">
-        <div style={{ marginTop: '2rem', position: 'static' }}>
-          <h1 style={{ fontSize: '3rem', fontWeight: 'bold', marginLeft: 'em', marginTop: 'em' }} className='text-black '>Welcome</h1>
-          <p style={{ marginLeft: 'em', padding: '1em',fontSize: '1.4rem'}} className='text-black '>This site allows you to make</p>
-          <p style={{ padding: 'em',fontSize: '1.4rem'}} className='text-black '>purchases and pay using a QR code.</p>
-        </div>
-        <div style={{ borderRadius: '0.75rem', boxShadow: '2px 2px 4px 9px rgba(0, 0, 0, 0.5)', width: '20em', margin: '5rem auto', padding: '1rem', backgroundColor:'white'}}>
-          <div style={{ textAlign: 'center' }}>
-            <p style={{ fontSize: '1.5rem' }} className='text-black'>Let's Get Started</p>
-            <p className='text-black'>Connect instantly by scanning and paying.</p>
-          </div>
-          <div className="buttons" style={{ display: 'flex', flexDirection: 'column', gap: '1rem', textAlign: 'center' }}>
-            <p className='text-black'>Join now</p>
-            <Button onClick={handleRegisterClick} variant="outline" className="ml-20 px-4 py-4 w-32 border border-black text-black hover:bg-black hover:text-white transition duration-300">
-              Sign up
-            </Button>
-            <p className='text-black'>Already have an account?</p>
-            <Button onClick={handleLoginClick} variant="outline" className="ml-20 px-4 py-4 w-32 border border-black text-black hover:bg-black hover:text-white transition duration-300">
-              Login
-            </Button>
+  const handleMerchantClick = () => {
+    navigate('admin-login');
+  };
+
+  return (
+    <>
+      <div className="relative min-h-screen flex flex-col justify-center items-center text-white"
+        style={{ backgroundImage: `url('/images/hero/bg1.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+
+        {/* Main content */}
+        <div className="relative z-10 text-center">
+          <div className="max-w-lg mx-auto p-8">
+            <h1 className="text-4xl font-bold mb-4">Welcome</h1>
+            <p className="text-xl mb-8">This site allows you to make purchases and pay using a QR code.</p>
+
+            {/* Call to action buttons */}
+            {!isContentVisible ? (
+              <div className="flex flex-row w-full justify-evenly">
+                <Button onClick={handleRegisterClick} variant="outline" className="w-48 py-3 border hover:border-black text-black hover:bg-white hover:text-black transition duration-300">
+                  Sign up
+                </Button>
+                <Button onClick={handleLoginClick} variant="outline" className="w-48 py-3 border hover:border-black text-black hover:bg-white hover:text-black transition duration-300">
+                  Login
+                </Button>
+              </div>
+            ) : (
+              <div className="flex flex-col items-center">
+                {content} {/* Display the content (Login or Signup form) */}
+                <Button onClick={handleBackClick} variant="outline" className="py-3 mt-4 px-6 border border-white text-black hover:bg-white hover:text-black transition duration-300">
+                  Back
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </div>
-    {isContentVisible && (
-      <div className="register-content">
-        {content}
-      </div>
-    )}
-  </div>
-  <Footer />
-  </div>
-);
+
+      {/* Espace Marchand Button */}
+      <button
+        onClick={handleMerchantClick}
+        className="absolute top-4 right-4 px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-700 transition duration-300 z-20"
+      >
+        Espace Marchand
+      </button>
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
 };
 
-export default index;
+export default Index;
